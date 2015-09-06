49d856f80b462baf09f4f6d70a841dc69f1feb94
Xavier Cambar <xcambar@gmail.com>
2015-09-06 18:42:21 +0200

    Generates a version file

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 /* jshint node: true */
 'use strict';
 
+var writeFile = require('broccoli-file-creator');
+var mergeTrees = require('broccoli-merge-trees');
+var appVersion = require('ember-cli-app-version');
+
 module.exports = {
-  name: 'ember-cli-self-update'
+  name: 'ember-cli-self-update',
+  config: function (env/* , config */) {
+    var versionInfo = { APP: {} };
+    appVersion.config.call(this, env, versionInfo);
+    return versionInfo;
+  },
+  treeForPublic: function (tree) {
+    var version = this.config().APP.version;
+    var versionFile = writeFile('/version.json', version);
+    return mergeTrees([tree, versionFile].filter(Boolean));
+  }
 };
