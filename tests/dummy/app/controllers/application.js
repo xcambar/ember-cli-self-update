8df9fad24676e7ea3bf44ddcc999f7ab9bfc6743
Xavier Cambar <xcambar@gmail.com>
2015-09-06 23:29:07 +0200

    Dummy app doesn't use prototype extension
    And I finally understood it

--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -4,7 +4,7 @@ export default Ember.Controller.extend({
   selfupdate: Ember.inject.service(),
   version: Ember.computed.alias('selfupdate.version'),
   hasUpdate: Ember.computed.alias('selfupdate.hasUpdate'),
-  init: function () {
+  didInit: Ember.on('init', function () {
     this.get('selfupdate').watchUpdates();
-  }
+  })
 });
