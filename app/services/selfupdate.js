df9e51e59c057c8e4ff12e9989b6cb82579b0a57
Xavier Cambar <xcambar@gmail.com>
2015-11-30 20:32:57 +0100

    Allows to disallow auto updates with configuration
    Useful to disable the selfupdate while testing an app

--- app/services/selfupdate.js
+++ app/services/selfupdate.js
@@ -2,8 +2,18 @@ import Ember from 'ember';
 import ENV from '../config/environment';
 import SelfUpdateService from 'ember-cli-self-update/services/selfupdate';
 
-const version = Ember.get(ENV, 'APP.version');
+const appConfig = Ember.get(ENV, 'APP');
+const version = Ember.get(appConfig, 'version');
+// Here we test also the existence of the key.
+// If the key is not available, we allow self update
+// for backward compatibility
+const allowSelfUpdate = !appConfig.hasOwnProperty('allowSelfUpdate') || Ember.get(appConfig, 'allowSelfUpdate');
 
 export default SelfUpdateService.extend({
   version: Ember.computed(()=> version).readOnly(),
+  watchUpdates() {
+    if (allowSelfUpdate) {
+      return this._super(...arguments);
+    }
+  }
 });
